245687f4095013e25954c6feff71b8e5003f36e7
Dan Page <dan@phoo.org>
2019-09-30 15:18:32 +0100

    update scripts, now w. license to match Vagrantfile

--- vagrant/provision_prologue.sh
+++ vagrant/provision_prologue.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright (C) 2017 Daniel Page <csdsp@bristol.ac.uk>
+#
+# Use of this source code is restricted per the CC BY-NC-ND license, a copy of 
+# which can be found via http://creativecommons.org (and should be included as 
+# LICENSE.txt within the associated archive or repository).
+
 # package manager: update
 sudo apt-get --quiet --assume-yes update
 sudo apt-get --quiet --assume-yes upgrade
--- vagrant/provision_unit.sh
+++ vagrant/provision_unit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Copyright (C) 2017 Daniel Page <csdsp@bristol.ac.uk>
+#
+# Use of this source code is restricted per the CC BY-NC-ND license, a copy of 
+# which can be found via http://creativecommons.org (and should be included as 
+# LICENSE.txt within the associated archive or repository).
+
 # software install: packaged software
 sudo                                  apt-get --quiet --assume-yes install libpcap-dev
 sudo                                  apt-get --quiet --assume-yes install netcat
